b06b806f6e6de9a87697062c47a3d909f4e14513
Alexander Pruss <alexander.pruss@akelius.de>
2019-05-20 13:32:03 +0200

    Formatting

--- src/karma/KarmaParser.spec.ts
+++ src/karma/KarmaParser.spec.ts
@@ -94,14 +94,6 @@ describe("KarmaParser", () => {
             expect(karmaRequests[0]).to.eql(expectedRequest);
         });
 
-        it("wraps user reference string in < > symbols", () => {
-            const expectedRequest = new Karma("<@abc123>", 1);
-
-            const karmaRequests = parser.parseMessage("@Karmabot @abc123++");
-
-            expect(karmaRequests.length).to.equal(1);
-            expect(karmaRequests[0]).to.eql(expectedRequest);
-        });
     });
 
     describe("#prettifyKarmaRequests", () => {
